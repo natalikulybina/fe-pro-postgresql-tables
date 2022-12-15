a4cdc94cef9c3402d30a0c002c7fb94eee971d1e
nataliia <102330868+natalikulybina@users.noreply.github.com>
2022-12-15 09:41:08 +0200

    HM17

--- src/index.js
+++ src/index.js
@@ -23,8 +23,54 @@ export const createStructure = async () => {
   const client = initConnection();
   client.connect();
 
-  // Your code is here...
-  // Your code is here...
+  await client.query(`
+    CREATE TABLE users(
+      id SERIAL PRIMARY KEY,
+      name VARCHAR (30) NOT NULL,
+      date DATE NOT NULL DEFAULT CURRENT_DATE
+    );`);
+
+    await client.query(`
+    CREATE TABLE categories(
+      id SERIAL PRIMARY KEY,
+      name VARCHAR (30) NOT NULL
+    );`);
+
+    await client.query(`
+    CREATE TABLE authors(
+      id SERIAL PRIMARY KEY,
+      name VARCHAR (30) NOT NULL
+    ); `);
+
+    await client.query(`
+    CREATE TABLE books(
+      id SERIAL PRIMARY KEY,
+      title VARCHAR(30) NOT NULL,
+      userid INTEGER NOT NULL,
+      foreign key (userid) REFERENCES users (id) ON DELETE CASCADE,
+      authorid INTEGER NOT NULL,
+      foreign key (authorid) REFERENCES authors (id) ON DELETE CASCADE,
+      categoryid INTEGER NOT NULL,    
+      foreign key (categoryid) REFERENCES categories (id) ON DELETE CASCADE
+    );`);
+
+    await client.query(`
+    CREATE TABLE descriptions(
+      id SERIAL PRIMARY KEY,
+      description VARCHAR(10000) NOT NULL,
+      bookid INTEGER NOT NULL,
+      foreign key (bookid) REFERENCES books (id) ON DELETE CASCADE
+    );`);
+
+    await client.query(`
+    CREATE TABLE reviews(
+      id SERIAL PRIMARY KEY,
+      message VARCHAR(10000) NOT NULL,
+      userid INTEGER NOT NULL,
+      foreign key (userid) REFERENCES users (id) ON DELETE CASCADE,
+      bookid INTEGER NOT NULL,
+      foreign key (bookid) REFERENCES books (id) ON DELETE CASCADE
+    );`);
 
   client.end();
 };
@@ -33,7 +79,29 @@ export const createItems = async () => {
   const client = initConnection();
   client.connect();
 
-  // Your code is here...
+  await client.query(`
+    INSERT INTO users(name) VALUES('Natali');
+  `);
+
+  await client.query(`
+    INSERT INTO categories(name) VALUES('High-fantasy novel');
+  `);
+
+  await client.query(`
+    INSERT INTO authors(name) VALUES('J. R. R. Tolkien');
+  `);
+
+  await client.query(`
+    INSERT INTO books(name, userid, authorid, categoryid) VALUES ('The Lord of the Rings', 1, 1, 1);
+  `);
+
+  await client.query(`
+    INSERT INTO descriptions(description, bookid) VALUES ('It explains how the narrative follows on from The Hobbit, in which the hobbit Bilbo Baggins finds the One Ring, which had been in the possession of Gollum.', 1);
+  `);
+
+  await client.query(`
+    INSERT INTO reviews(message, userid, bookid) VALUES ('Love him and his son's books - greatest fantasy of all time.', 1, 1);
+  `);
 
   client.end();
 };
